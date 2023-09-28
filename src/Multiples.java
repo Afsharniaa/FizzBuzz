3ccd82bbcddf91a243d034bb5105b4abfeda630f
sahba <sahba.afsharnia@mail.utoronto.ca>
2023-09-27 22:03:13 -0400

    multiples

---
+++ src/Multiples.java
@@ -0,0 +1,13 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+    int ii = 0;
+    for (int i = 1; i < 100; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+        ii ++;
+        }}
+            System.out.println(ii);
+
+}}
+
